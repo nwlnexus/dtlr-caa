b8765406619106750fa05dcbc462847538510360
Nigel Williams-Lucas <nwilliams-lucas@dtlr.com>
2018-03-19 00:39:55 -0400

    Added function to create the user and mailbox at once.

--- caa.ps1
+++ caa.ps1
@@ -3,16 +3,8 @@ Function Get-RandomPassword {
 		[int]$Length = 10
 	)
 
-	$ascii = $NULL
-	For ($a = 48; $a -le 122; $a++) {
-		$ascii +=,[char][byte]$a
-	}
-
-	For ($loop = 1; $loop -le $Length; $loop++) {
-		$tmpPassword += ($ascii | Get-Random)
-	}
-
-	return $tmpPassword
+	$wordList = Get-Content -Path ".\wordlist.txt"
+	$randomWordArrayget = [array](Get-Random -Maximum ([array]$wordList.count))
 }
 
 Function Add-DTLRAccounts {
@@ -37,5 +29,58 @@ Function Add-DTLRAccounts {
 		 -GivenName $Acct.dtlr_key -Surname $Acct.store_long -UserPrincipalName $UPN -Path "$OU"
 		 -PasswordNeverExpires $true -ChangePasswordAtLogon $false -CannotChangePassword $true -Department "Operations"
 		 -StreetAddress $Acct.store_address -PostalCode $Acct.store_zip -State $Acct.store_state -City $Acct.store_city
+	}
+}
+
+Function Add-DTLRAccountsExchange {
+	Param (
+		[Parameter(Mandatory=$true)]
+		[ValidateScript({ Test-Path -Path $_ -PathType Leaf })]
+		[ValidatePattern('\.csv$')]
+		[string]$File
+	)
+
+	$Accts = Import-Csv -Path $File -Delimiter ","
+	$SourceGroups = Get-ADUser "015" -Property MemberOf | ForEach-Object {
+		$_.MemberOf | Get-ADGroup | Select-Object Name -ExpandProperty Name | sort name
+	}
+
+	ForEach ($Acct in $Accts) {
+		$storeNumber = $Acct.dtlr_key
+		$storeFirstName = $Acct.dtlr_key
+		$storeLastName = $Acct.store_long
+		$storeDisplayName = $storeFirstName + " " + $storeLastName
+		$OU = "OU=Villa,OU=Stores,OU=Users,OU=Corporate Office,OU=DTLR,DC=levtrannt,DC=lan"
+		$UPN = $Acct.dtlr_key + "@dtlr.com"
+
+		$password = ""
+
+		New-Mailbox
+			-UserPrincipalName $UPN
+			-Password
+			-Database "DB_Stores"
+			-Name "$storeDisplayName"
+			-DisplayName "$storeDisplayName"
+			-SamAccountName $Acct.dtlr_key
+			-FirstName $Acct.dtlr_key
+			-LastName $Acct.store_long
+			-OrganizationalUnit "$OU"
+			-RetentionPolicy "DTLR_14_Day"
+		 	-ResetPasswordOnNextLogon $false
+
+		Get-ADUser -Filter "UserPrincipalName -eq '$UPN'" |
+			Set-ADUser -Replace @{
+				Department = "Operations";
+				PasswordNeverExpires = $true;
+				CannotChangePassword = $true;
+				StreeAddress = $Acct.store_address;
+				PostalCode = $Acct.store_zip;
+				State = $Acct.store_state;
+				City = $Acct.store_city;
+			}
+
+		ForEach ($Group in $SourceGroups) {
+			Add-ADGroupMember $Group -Member $storeNumber
+		}
 	}
 }
